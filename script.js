ac2d33cea16c470c3d7045f6178b65df7acf97ca
marcopintus <marco.pintus93@gmail.com>
2022-06-08 12:55:57 +0200

    Added the five rounds game with player input via prompt

--- script.js
+++ script.js
@@ -14,28 +14,66 @@ function playRound(playerSelection,computerSelection){
     /* rock-paper-scissors logic implementation */
     if (playerSelection==computerSelection){
         return "Draw"
-    } else if (computerSelection=="rock") {
-        if (playerSelection=="paper"){
-            return "You Win! Paper beats Rock";
-        } else if (playerSelection=="scissors"){
-            return "You Lose! Rock beats Scissors";
-        }
-    } else if (computerSelection=="paper"){
-        if (playerSelection=="scissors"){
-            return "You Win! Scissors beat Paper";
-        } else if (playerSelection=="rock"){
-            return "You Lose! Paper beat Rock";
-        }
-    } else if (computerSelection=="scissors") { 
-        if (playerSelection=="rock"){
+    } else if (playerSelection=="rock") {
+        if (computerSelection=="paper"){
+            return "You Lose! Paper beats Rock";
+        } else if (computerSelection=="scissors"){
             return "You Win! Rock beats Scissors";
-        } else if (playerSelection=="paper"){
-            return "You Lose! Scissors beat Paper";
         }
+    } else if (playerSelection=="paper"){
+        if (computerSelection=="scissors"){
+            return "You Lose! Scissors beat Paper";
+        } else if (computerSelection=="rock"){
+            return "You Win! Paper beats Rock";
+        }
+    } else if (playerSelection=="scissors") { 
+        if (computerSelection=="rock"){
+            return "You Lose! Rock beats Scissors";
+        } else if (computerSelection=="paper"){
+            return "You Win! Scissors beat Paper";
+        }
+    } else if(playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissors" ){
+        return "Wrong Player input"; /* this will be deleted with the graphical interface */
     }
 }
 
-let playerSelection = "rock".toLowerCase();
-let computerSelection = computerPlay().toLowerCase();
+function game() { 
+    /* play a 5 round game of rock-paper-scissors */
 
-console.log("result: ", playRound(playerSelection,computerSelection))
+    /* initialize the score */
+    let playerScore = 0;
+    let cpuScore = 0;
+
+    /* first to three wins: playerScore < 3 && cpuScore < 3 */
+    /* five rounds */
+    for(let i=0;i<5;i++){
+
+        /* player and computer selections */
+        let playerSelection = prompt("Rock-Paper-Scissor?").toLowerCase();
+        let computerSelection = computerPlay().toLowerCase();
+
+        let result = playRound(playerSelection,computerSelection)
+        console.log(result)
+        if (result[4]=="W"){
+            playerScore++;
+        } else if(result[4]=="L"){
+            cpuScore++;
+        }
+        console.log("Live scores: ");
+        console.log("Player: ",playerScore);
+        console.log("Computer: ",cpuScore);
+
+        if (playerScore>cpuScore){
+            console.log("You've Won!");
+        } else if(cpuScore>playerScore){
+            console.log("You've Lost!");
+        } else{
+            console.log("It's a Draw!");
+        }
+    }
+
+
+}
+
+
+game()
