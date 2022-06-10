b9284bafd9b175d490aa6840f5719994b088ac27
marcopintus <marco.pintus93@gmail.com>
2022-06-10 02:14:45 +0200

    Improved UI, added a working 'Play Again' button

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* queries */
+const plrScr = document.querySelector("#plr-scr");
+const cpuScr = document.querySelector("#cpu-scr");
+const plrChoice = document.querySelector("#my-sel");
+const cpuChoice = document.querySelector("#cpu-sel");
+const res = document.querySelector("#res");
+const mainButtons = document.querySelectorAll(".main-btn");
+
+
+/* functions */
 
 function computerPlay(){
     /* generates the random output of the computer play */
@@ -6,9 +16,8 @@ function computerPlay(){
 }
 
 function updateScoreUI(playerScore,cpuScore){
-    const plrScr = document.querySelector("#plr-scr");
+    /* updates the score on the web page */
     plrScr.textContent = `${playerScore}`
-    const cpuScr = document.querySelector("#cpu-scr");
     cpuScr.textContent = `${cpuScore}`
 }
 
@@ -18,14 +27,11 @@ function playRound(playerSelection,computerSelection){
     /* stating the player choice and computer choice */
     console.log("Player: ",playerSelection);
     console.log("Computer: ", computerSelection);
-
-    const plrChoice = document.querySelector("#my-sel");
+    /* writing the player and cpu choices on the web page */
     plrChoice.textContent= `${playerSelection[0].toUpperCase()+playerSelection.slice(1)}`;
     plrChoice.setAttribute("style","text-align:center;");
-    const cpuChoice = document.querySelector("#cpu-sel");
     cpuChoice.textContent= `${computerSelection[0].toUpperCase()+computerSelection.slice(1)}`;
     cpuChoice.setAttribute("style","text-align:center;");
-
     /* rock-paper-scissors logic implementation */
     if (playerSelection==computerSelection){
         return "Draw"
@@ -51,59 +57,98 @@ function playRound(playerSelection,computerSelection){
 }
 
 function scoreCounter(result){
+    /* assigns a point to the winner of the round according to the r-p-s logic*/
     if (result[4]=="W"){
         playerScore++;
     } else if(result[4]=="L"){
         cpuScore++;
     }
+    /* prints results to the console for debugging */
     console.log("Live scores: ");
     console.log("Player: ",playerScore);
     console.log("Computer: ",cpuScore);
-
+    /* writes the updated score on the web page*/
     updateScoreUI(playerScore,cpuScore);
 }
 
+function resContCreation(){
+    /* creates the containers for the final result of the match */
+    const cntResCnt = document.createElement('div');
+    cntResCnt.classList.add("cont-res-cont");
+    document.body.appendChild(cntResCnt);
+    const resContainer = document.createElement('div')
+    resContainer.classList.add("res-container");
+    cntResCnt.appendChild(resContainer);
+}
 
-function gameWinner() {
-    if (playerScore>cpuScore){
-        console.log("You've Won the match!");
-        let resContainer = document.querySelector('.res-container');
-        resContainer.textContent = "You've Won the match!"
-        resContainer.setAttribute("style", "text-align:center;");
-    } else if(cpuScore>playerScore){
-        console.log("You've Lost the match!");
-        let resContainer = document.querySelector('.res-container');
-        resContainer.textContent = "You've Lost the match!"
-        resContainer.setAttribute("style", "text-align:center;");
-    } else{
-        console.log("It's a Draw!");
-    }
+function gameReset(){
+    /* resets the game to the original status */
     playerScore = 0;
     cpuScore = 0;
-    updateScoreUI(playerScore,cpuScore)
+    updateScoreUI(playerScore,cpuScore);
+    const contResCont = document.querySelector(".cont-res-cont");
+    contResCont.remove()
+    plrChoice.textContent= "-";
+    cpuChoice.textContent= "-";
+    res.textContent = "-";
+}
+
+function endGamePop(resContainer){
+    /* styles a div that shows the final result of the game and allows to play another match */
+    resContainer.setAttribute("style", "width:800px; height:650px; margin-top:-900px; border:16px solid azure; border-radius: 24px;");
+    const playAgainBtn = document.createElement('button');
+    playAgainBtn.classList.add("play-again");
+    playAgainBtn.textContent = "Play Again"
+    resContainer.appendChild(playAgainBtn)
+    playAgainBtn.addEventListener('click',gameReset)
+
+}
+
+function gameWinner() {
+    /* returns the winner of the match by displaying it on the final result div */
+    if (playerScore>cpuScore){
+        console.log("You've Won the match!");
+        resContCreation()
+        const resContainer  = document.querySelector('.res-container');
+        resContainer.textContent = "You've Won the match!"
+        endGamePop(resContainer)
+    } else if(cpuScore>playerScore){
+        console.log("You've Lost the match!");
+        resContCreation()
+        const resContainer  = document.querySelector('.res-container');
+        resContainer.textContent = "You've Lost the match!"
+        endGamePop(resContainer)
+    }
 }
 
 
+/* variables initilaisation */
 let playerScore = 0;
 let cpuScore = 0;
 let computerSelection
-const mainButtons = document.querySelectorAll(".main-btn");
 
+/* game */
 mainButtons.forEach((button)=> {
+    /* for each button an event listener is employed */
     button.addEventListener('click', () => {
-        let playerSelection = button.id
-        console.log("player selection: ",playerSelection)
-        let result = playRound(playerSelection,computerSelection)
-        
-        console.log(result)
-        const res = document.querySelector("#res");
-        res.textContent = `${result}`;
-        scoreCounter(result)
-        if (playerScore ==3 || cpuScore == 3){
-            gameWinner()
-        }
+                /* the player selection equals the clicked button id */
+                let playerSelection = button.id
+                console.log("player selection: ",playerSelection)
+                /* with the player selection, a round is played */
+                let result = playRound(playerSelection,computerSelection)
+                console.log(result)
+                /* the result of hte round is then printed on the page */
+                const res = document.querySelector("#res");
+                res.textContent = `${result}`;
+                /* then the score is updated */
+                scoreCounter(result)
+                if (playerScore ==3 || cpuScore == 3){
+                    /* the first to reach 3 points between player and cpu wins */
+                    gameWinner()
+                }
+            }
+        ) 
     }
-    ) }
-    );
+);
 
     
